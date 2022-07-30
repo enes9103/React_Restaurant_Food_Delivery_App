9a8192128f9b98401df289f9f3ba7a149d443ee1
Enes ÜNLÜ <89146799+enes9103@users.noreply.github.com>
2022-07-30 16:24:08 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,8 @@ const Header = () => {
     <div className="fixed z-50 w-screen bg-slate-300 p-6 px-16">
         {/* desktop & tablet */}
         <div className="hidden md:flex w-full h-full">
-
         </div>
 
-        {/* mobile */}
         <div className="flex md:hidden w-full h-full">
 
         </div>
